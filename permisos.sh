5de1d2c346f4b2cc03411ed76dad99fd2e51b17b
Henry <henry.burgos@complexless.com>
2023-05-20 17:45:43 -0500

    Agregando ejecutable de permisos

---
+++ permisos.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+chmod +x menu.sh
+chmod +x script1.sh
+chmod +x script2.sh
+chmod +x script3.sh
+chmod +x script4.sh
+chmod +x script5.sh
+chmod +x script6.sh
+chmod +x script7.sh
+chmod +x script8.sh
+chmod +x script9.sh
+chmod +x script10.sh
